cd0195b9ec78daa73457e13a0ae610e193b97db1
francis renauld <renauldf12@gmail.com>
2022-08-23 14:39:01 -0400

    Add give free NFT too the contract

--- contracts/RocketToken.sol
+++ contracts/RocketToken.sol
@@ -11,10 +11,23 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract RocketToken is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
     using Counters for Counters.Counter;
-    uint cost = 0.0001 ether; 
+    uint cost =  0.01 ether; 
     Counters.Counter private _tokenIdCounter;
+    mapping(address => bool) public freeNFTList;
 
-    constructor() ERC721("RocketToken", "Rocket") {}
+    constructor() ERC721("RocketToken", "Rocket") {            
+        freeNFTList[0x92A22470b1eC3DE435Da89E9f0B7183cEB2f3714] = true;
+        freeNFTList[0x004660331dd96BfE95Ad4D32bf5EF7845a0Bc689] = true;
+        freeNFTList[0xe578a5896931207AEb909Ba12EFc92b88422950a] = true;
+        freeNFTList[0x4221ab4A5A1172FF48bB385d38fBa33453427957] = true;
+        freeNFTList[0x4eeA12a0B97af76FaBDC91b76B1960173DC15e89] = true;
+        freeNFTList[0x6c9e61363F48fEAc41f9a2469E54994Ce745E274] = true;
+        freeNFTList[0xDf6EfD03c762374c1a840558dD9d3554B4fa3658] = true;
+        freeNFTList[0x326a276b46BAaD8d1019fEd693B02ED0ad82FA01] = true;
+        freeNFTList[0x78aDae76DB2FcC462Fe4AD58Eb2ED87a1bF05F9f] = true;
+        freeNFTList[0x71c52f19d1cd0bC0a92663ab211E20a7a31Ad5BC] = true;
+
+        }
 
     function pause() public onlyOwner {
         _pause();
@@ -24,12 +37,30 @@ contract RocketToken is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ow
         _unpause();
     }
 
+        function giveFreeNFT(address to, string memory uri) public payable{
+        require(freeNFTList[to] == true, "You allready have your free NFT or your address is not permit to get a free NFT");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        freeNFTList[to] = false;
+    }
+
     function safeMint(address to, string memory uri) public payable{
         require(msg.value >= cost, "Not enough ETH was sent");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+    }
+
+        function freeNFT(address to, string memory uri) public {
+
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
